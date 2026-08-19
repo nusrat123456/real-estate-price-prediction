598c3d1538ff541a0b43e6c0bef30bda3e1f4969
nusrat123456 <113042320+nusrat123456@users.noreply.github.com>
2026-08-19 20:08:26 +0530

    Fix Django static files

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 cd Backend
-
 python manage.py collectstatic --noinput
-python manage.py migrate
