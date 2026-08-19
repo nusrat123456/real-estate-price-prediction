2c7ac6c1e1c482ad1d2c33ff1ede9be2e53b678e
nusrat123456 <113042320+nusrat123456@users.noreply.github.com>
2026-08-19 19:39:34 +0530

    Fix HttpResponse import

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Real Estate Price Prediction API is Live!")
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("predictions/", include("predictions.urls")),
