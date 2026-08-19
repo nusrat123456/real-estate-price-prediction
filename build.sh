d8d909ec33fea1d092096aadbd5ebbf9ca991025
nusrat123456 <113042320+nusrat123456@users.noreply.github.com>
2026-08-19 18:28:03 +0530

    Fix Render build script

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -o errexit
 
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 cd Backend
 
 python manage.py collectstatic --noinput
-
 python manage.py migrate
